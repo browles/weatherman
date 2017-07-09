ec2b651ba1329dc95640acfffcf95855b4c26bd7
browles <rowlesben@gmail.com>
2017-07-08 21:54:28 -0700

    init

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject trader "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.6.1"]
+                 [cheshire "5.7.1"]
+                 [org.clojure/java.jdbc "0.6.1"]
+                 [org.xerial/sqlite-jdbc "3.19.3"]
+                 [org.knowm.xchange/xchange-core "4.2.0"]
+                 [org.knowm.xchange/xchange-poloniex "4.2.0"]
+                 [org.knowm.xchange/xchange-coinbase "4.2.0"]]
+  :main ^:skip-aot trader.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/trader/core.clj
@@ -0,0 +1,22 @@
+(ns trader.core
+  (:require [clj-http.client :as http])
+  (:import [org.knowm.xchange BaseExchange Exchange ExchangeFactory ExchangeSpecification]
+           [org.knowm.xchange.currency Currency CurrencyPair]
+           [org.knowm.xchange.poloniex Poloniex PoloniexAdapters PoloniexAuthenticated PoloniexException PoloniexExchange PoloniexUtils]
+           [org.knowm.xchange.poloniex.service PoloniexMarketDataService])
+  (:gen-class))
+
+(def poloniex (.createExchange ExchangeFactory/INSTANCE (.getName PoloniexExchange)))
+(def poloniex-marketdata (.getMarketDataService poloniex))
+
+(def poloniex-loan-endpoint "https://poloniex.com/public?command=returnLoanOrders&currency=")
+
+(defn get-loan-orders [currency-string]
+  (->
+    (http/get (str poloniex-loan-endpoint currency-string) {:as :json})
+    :body))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
